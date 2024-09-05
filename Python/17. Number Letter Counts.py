ceef5ac12ee8e1467de8e9b8d697f2084faaafd6
Josh Birch <2312292@chester.ac.uk>
2024-09-05 08:28:52 +0100

    Built prerequisite for problem 17

--- Python/17. Number Letter Counts.py
+++ Python/17. Number Letter Counts.py
@@ -16,10 +16,18 @@
 
 
 
-1 = "one"
-2 = "two"
-3 = "three"
-4 = "four"
-5 = "five"
+num_list = []
+word_dict = {
+    "one":1,
+    "two":2,
+    "three":3,
+    "four":4,
+    "five":5,
+}
 
-for letter in 
+total = 0
+
+for string in word_dict.keys():
+    total = total + (len(string))
+print(total)
+
