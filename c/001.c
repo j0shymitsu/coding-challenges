a5fdba155f6eae1aa4596f7d89eff468b436c985
Josh Birch <joshuabirch@protonmail.com>
2025-08-26 10:59:30 +0100

    Finish problem 1 in c

---
+++ c/001.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main()
+{
+    int total = 0;
+    int i = 0;
+
+    for (i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            total += i;
+        }
+        
+    }
+    
+    printf("%d", total);
+
+    return 0;
+    
+}
