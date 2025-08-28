4dd44db0e3b119bbc11dd1c33c29b7972b681e7e
Josh Birch <joshuabirch@protonmail.com>
2025-08-28 13:56:02 +0100

    Start problem 3 "Largest Prime Factor" in C (unfinished); add .gitignore

---
+++ c/003.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+long[] return_primes(long[] num_list);
+bool is_prime(long x);
+
+int main(void)
+{
+    return 0;
+}
+
+
+long[] return_primes(long[] num_list)
+{
+    for (size_t i = 0; i < sizeof(prime_numbers); i++)
+    {
+        if (is_prime(i))
+        {
+            printf("\n")
+        }
+    }
+}
+
+bool is_prime(long x)
+{
+    return 0;
+}
+
+    
