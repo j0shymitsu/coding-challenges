e1e910cb84ba98835b25b720f5cca29448c61fd2
Josh Birch <joshuabirch@protonmail.com>
2025-09-04 15:41:20 +0100

    Add work to problem 11 in Python (unfinished)

--- c/003.c
+++ c/003.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
-long[] return_primes(long[] num_list);
-bool is_prime(long x);
+bool is_prime(int n);
+int[] factors_of(int n)
 
 int main(void)
 {
+    int n = 13195;
+    
     return 0;
 }
 
 
-long[] return_primes(long[] num_list)
-{
-    for (size_t i = 0; i < sizeof(prime_numbers); i++)
-    {
-        if (is_prime(i))
-        {
-            printf("\n")
-        }
-    }
-}
 
-bool is_prime(long x)
-{
-    return 0;
-}
 
     
