ac5cccda538009eaa3d9258551b407fa75ead6a7
Josh Birch <joshuabirch@protonmail.com>
2025-10-21 16:35:01 +0100

    Start Euler Project in CPP, complete problem 1

---
+++ project_euler/cpp/multiples_of_3_or_5.cpp
@@ -0,0 +1,29 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
+
+// Find the sum of all multiples of 3 or 5 below 1000.
+
+#include <iostream>
+using namespace std;
+
+int sum_multiples_of_three_or_five(int limit);
+
+int main()
+{
+	cout << sum_multiples_of_three_or_five(1000) << endl;	
+	return 0;
+}
+
+int sum_multiples_of_three_or_five(int limit)
+{
+	int sum = 0;
+
+	for (size_t i = 0; i < limit; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+
+	return sum;
+}
