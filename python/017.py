c5b3788b21256139e9b95a1281299ddb3042b0f3
Josh Birch <joshuabirch@protonmail.com>
2026-01-09 06:36:10 +0000

    Complete problem 17 "Number Letter Counts" in Python

---
+++ python/017.py
@@ -0,0 +1,88 @@
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are:
+
+    # 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+# If all the numbers from 1 to 1000 were written out in words, how many letters would be used?
+
+# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 
+# 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of 'and' when writing
+# out numbers is in compliance with British usage.
+
+
+
+############################
+### NUMBER LETTER COUNTS ###
+############################
+
+
+
+num_to_word = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+    100: "onehundred",
+    200: "twohundred",
+    300: "threehundred",
+    400: "fourhundred",
+    500: "fivehundred",
+    600: "sixhundred",
+    700: "sevenhundred",
+    800: "eighthundred",
+    900: "ninehundred",
+    1000: "onethousand",
+}
+
+def number_to_word(n):
+    if n in num_to_word:
+        return num_to_word[n]
+    
+    if 21 <= n <= 99:
+        tens = (n // 10) * 10
+        units = n % 10
+
+        return num_to_word[tens] + num_to_word[units]
+    
+    if 100 < n < 1000:
+        hundreds = (n // 100) * 100
+        remainder = n % 100
+
+        return num_to_word[hundreds] + "and" + number_to_word(remainder)
+    
+    raise ValueError(f'Number out of range: {n}')
+
+total = 0
+
+for i in range(1, 1001):
+    word = number_to_word(i)
+    total += len(word)
+
+print(total)
+
+
+
+
+
