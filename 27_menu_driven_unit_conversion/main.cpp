7db88b0e0e401eabc006ea5e9f39ae7d59deb93d
Josh Birch <joshuabirch@protonmail.com>
2026-01-24 10:08:14 +0000

    Finish program 27 "Menu Driven Unit Converter"

--- 27_menu_driven_unit_conversion/main.cpp
+++ 27_menu_driven_unit_conversion/main.cpp
@@ -12,14 +12,14 @@ int main() {
     while (true)
     {
       // Menu
-      std::cout << "Do you want to convert from metric (1)," 
+      std::cout << "\nDo you want to convert from metric (1)," 
                 << "or from imperial (2)? (Enter 4 to exit)\n";
 
       int conversion_direction = 0;
 
       while (true) 
       {
-        conversion_direction = readInt("(Enter 1 or 2): ");
+        conversion_direction = readInt("(Enter 1, 2 or 4): ");
         if (conversion_direction == 1) { break; } 
         if (conversion_direction == 2) { break; }
         if (conversion_direction == 4) { break; }
@@ -29,7 +29,7 @@ int main() {
 
       if (conversion_direction == 4) { break; }
 
-      std::cout << "1: Centimeters and inches\n"
+      std::cout << "\n1: Centimeters and inches\n"
                 << "2: Meters and feet\n"
                 << "3: Kilometers and miles\n"
                 << "4: Exit\n\n";
@@ -39,60 +39,70 @@ int main() {
 
       if (conversion_direction == 1)
       {
+        double result = 0.0;
+
         switch (choice) 
         {
           case 1:
           {
             int cm = readInt("Enter centimetres: "); 
-            double result = centimetreConverter(cm, conversion_direction);
-            return result;
+            result = centimetreConverter(cm, conversion_direction);
+            std::cout << cm << " cm = " << result << " inches\n";
+            break;
           }
           case 2:
           {
             int m = readInt("Enter metres: ");
-            double result = metreConverter(m, conversion_direction);
-            return result;
+            result = metreConverter(m, conversion_direction);
+            std::cout << m << " m = " << result << " feet\n";
+            break;
           }
           case 3:
           {
             int km = readInt("Enter kilometres: ");
-            double result = kilometreConverter(km, conversion_direction);
-            return result;
+            result = kilometreConverter(km, conversion_direction);
+            std::cout << km << " km = " << result << " miles\n"; 
+            break;
           }
           default:
           {
             std::cout << "Error. Please try again.";
-            return -1;
+            continue;
           }
         }
       }
 
       if (conversion_direction == 2)
       {
+        double result = 0.0;
+
         switch (choice)
         {
           case 1:
           {
             int inches = readInt("Enter inches: ");
-            double result = centimetreConverter(inches, conversion_direction);
-            return result;
+            result = centimetreConverter(inches, conversion_direction);
+            std::cout << inches << " inches = " << result << " cm\n";
+            break;
           }
           case 2:
           {
             int feet = readInt("Enter feet: ");
-            double result = metreConverter(feet, conversion_direction);
-            return result;
+            result = metreConverter(feet, conversion_direction);
+            std::cout << feet << " feet = " << result << " m\n";
+            break;
           }
           case 3:
           {
             int miles = readInt("Enter miles: ");
-            double result = kilometreConverter(miles, conversion_direction);
-            return result;
+            result = kilometreConverter(miles, conversion_direction);
+            std::cout << miles << " miles = " << result << " km\n";
+            break;
           }
           default:
           {
             std::cout << "Error. Please try again.";
-            return -1;
+            continue;
           }
         }
       }
